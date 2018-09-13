3c299378b1dd29b585bbc1a596bc0d16071169ed
Frank Wei <frank@frankwei.xyz>
2018-09-13 14:31:04 +0800

    go

--- 201409/201409-3-字符串匹配/main.cpp
+++ 201409/201409-3-字符串匹配/main.cpp
@@ -3,15 +3,17 @@
 
 using namespace std;
 
-void run(string str, string key, int op)
+void runLocateStr(string str, string key, int op)
 {
     string locstr = str;
     if (op == 0)
     {
+        // 如 op 为0，则全部转化为小写来匹配
         transform(str.begin(), str.end(), locstr.begin(), ::tolower);
         transform(key.begin(), key.end(), key.begin(), ::tolower);
     }
 
+    // 如果没到字符串末端则匹配成功
     if (locstr.find(key) != string::npos)
     {
         cout << str << endl;
@@ -24,13 +26,12 @@ int main(void)
     int op, N;
     cin >> key;
     cin >> op;
-    ;
     cin >> N;
 
     for (int i = 0; i < N; i++)
     {
         cin >> str;
-        run(str, key, op);
+        runLocateStr(str, key, op);
     }
 
     return 0;
